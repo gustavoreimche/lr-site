515737b0c50ff567cbc7d9510cd3d0a110f8fbb5
Gustavo Reimche <gustavoprdevgmail.com>
2023-07-26 13:37:19 -0300

    adicionando traduções

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, Output, EventEmitter } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-banner',
@@ -7,11 +8,20 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./banner.component.css'],
 })
 export class BannerComponent {
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private translate: TranslateService
+  ) {
+    this.translate.addLangs(['pt', 'esp']);
+    if (localStorage.getItem('lang')) {
+      this.translate.setDefaultLang(localStorage.getItem('lang') as string);
+    }
+  }
 
   @Output() inscricaoClick = new EventEmitter();
 
   isMobile: boolean = false;
+  lang: string = (localStorage.getItem('lang') as string) ?? 'pt';
 
   ngOnInit(): void {
     this.breakpointObserver.observe(Breakpoints.Handset).subscribe((result) => {
@@ -22,5 +32,10 @@ export class BannerComponent {
   onInscricaoClick(): void {
     console.log('Inscricao clicked');
     this.inscricaoClick.emit();
+  }
+
+  changeLang(): void {
+    localStorage.setItem('lang', this.lang);
+    this.translate.use(this.lang);
   }
 }
